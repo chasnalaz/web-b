dff5259623ff3748dc0b80b2830a8594f8ea4d25
chasnalaz <151990086+chasnalaz@users.noreply.github.com>
2023-12-01 10:54:26 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded ({ extended: false })
+const port = 3000
+
+
+app.use(cors())
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.get('/', (req, res) => {
+  res.json('website-a')
+})
+
+app.post("/github-event", (req, res) => {
+  if (req.body.secret !== "secret123") {
+    return res.status(403).json({ error: "Invalid secret" });
+  }
+
+  console.log("Incoming Webhook");
+  res.json("");
+});
+
+app.post("/github", (req, res) => {
+  if (req.body.secret !== "secret123") {
+    return res.status(403).json({ error: "Invalid secret" });
+  }
+
+  console.log("Incoming Webhook");
+  res.json("");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
